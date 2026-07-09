171eb6f8487eef958a9acb69a58d7e9679a53abb
irinalauritto <irina.lauritto@ingenieria.uner.edu.ar>
2026-07-09 18:27:05 -0300

    Se agregan comentarios en la interfaz de emg

--- interfaz_emg/src/interfazEmg.py
+++ interfaz_emg/src/interfazEmg.py
@@ -3,10 +3,11 @@ emg_plot.py
 
 Visualización en tiempo real de la señal EMG (ENV) recibida desde la ESP32
 por puerto serie.
-Adquisición, graficación y filtrado por promedios móviles de la señal EMG (ENV) en tiempo real..
+Adquisición, graficación y  procesamiento  de la señal EMG (ENV) en tiempo real.
 
 """
 
+# Importación de librerías
 import sys
 from collections import deque
 
@@ -15,24 +16,24 @@ import pyqtgraph as pg
 from PySide6.QtCore import QTimer
 from PySide6.QtWidgets import QApplication
 
-# --- Configuración ---
+# Definción de constantes globales
 SERIAL_PORT = "COM8"   # Ajustar según el puerto asignado a la ESP32 (Administrador de dispositivos de Windows)
 BAUD_RATE = 115200
 BUFFER_SIZE = 2000    # Cantidad de muestras visibles en el gráfico
-UPDATE_MS = 1           # Intervalo de refresco del gráfico (ms)
+UPDATE_MS = 20       # Intervalo de refresco del gráfico (ms)
 
 
 def main():
-    app = QApplication(sys.argv)
+    app = QApplication(sys.argv) #Creación de la aplicación Qt
 
-    # --- Conexión serie ---
+    # Apertura del puerto serie
     try:
         ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=0)
     except serial.SerialException as e:
         print(f"No se pudo abrir el puerto {SERIAL_PORT}: {e}")
         sys.exit(1)
 
-    # --- Ventana y gráfico ---
+    # Grafico en tiempo real usando PyQtGraph
     win = pg.GraphicsLayoutWidget(show=True, title="Señal EMG en tiempo real")
     win.resize(900, 500)
     plot = win.addPlot(title="MyoWare 2.0 - ENV")
@@ -42,6 +43,7 @@ def main():
     plot.enableAutoRange(axis="y")
     curve = plot.plot(pen=pg.mkPen(color="g", width=1))
 
+    # Inicialización del buffer circular para almacenar las muestras de la señal EMG
     buffer = deque([0] * BUFFER_SIZE, maxlen=BUFFER_SIZE)
 
     def update():
@@ -52,6 +54,7 @@ def main():
                 buffer.append(int(line))
         curve.setData(list(buffer))
 
+    # Configuración del temporizador para actualizar el gráfico
     timer = QTimer()
     timer.timeout.connect(update)
     timer.start(UPDATE_MS)
